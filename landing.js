048e6fc77296d9a88db939bea94e2cc9c7b98d43
lethern <lethern@o2.pl>
2019-08-29 03:31:32 +0200

    improved animation

--- landing.js
+++ landing.js
@@ -5,18 +5,16 @@ function onload(){
 	setupBackgroundSpecial();
 };
 
-var x=0;
 function setupBackgroundSpecial(){
-	if(x++ > 10) return;
 	console.log('setupBackgroundSpecial');
-	//let delay_ms = randomInRange( 3000, 6000 );
-	let delay_ms = randomInRange( 1000, 2000 );
+	let delay_ms = randomInRange( 3000, 6000 );
+	//let delay_ms = randomInRange( 1000, 2000 );
 	//let delay_ms = 1;
 	setTimeout( drawBackgroundSpecial, delay_ms );
 };
 
 function drawBackgroundSpecial(){
-	let wrapper = createDiv( document.body, ['animExpandRect', 'bckSpecial']);
+	let wrapper = createDiv( document.body, 'specialWrapper' );
 	wrapper.style.top = randomInRange(5, 75)+'%';
 	var horizontal = randomInRange(15, 85);
 	if(horizontal > 50){
@@ -24,9 +22,54 @@ function drawBackgroundSpecial(){
 	}else{
 		wrapper.style.left = (horizontal-15)+'%';
 	}
-	console.log('drawBackgroundSpecial');
 	
+	
+	let titlebar = createDiv( wrapper, ['animExpandRect', 'specialTitlebar'] );
+	
+	setTimeout( drawBackgroundSpecial_body, 650, {wrapper, titlebar} );
+};
+
+function drawBackgroundSpecial_body(divs){
+	divs.body = createDiv( divs.wrapper, ['animExpandRect', 'specialBody'] );
+	
+	setTimeout(drawBackgroundSpecial_action, 700, divs);
+};
+
+function drawBackgroundSpecial_action(divs){
+	//divs.body.textContent = 'hi\nhi';
+	
+	//var text = document.getElementById('text');
+	//var newDom = '';
+	const animationDelay = 350;
+	let text = getRandomText();
+	text = '<span>' + text.split('\n').join('</span><span>') + '</span>';
+	text = text.split(' ').join('&nbsp;');
+	
+	let fragment = document.createElement('div');
+	fragment.innerHTML = text;
+	
+	let children = fragment.children;
+	for(let i=0, len=children.length; i<len; ++i){
+		children[i].style['animation-delay'] = i*animationDelay + 'ms';
+	}
+	
+	divs.body.appendChild(fragment);
+
+	addClasses(divs.body, 'animFadeInText');
+	
+	let delay_ms = randomInRange( 3000, 6000 );
+	delay_ms += children.length * animationDelay;
+	setTimeout(drawBackgroundSpecial_hide, delay_ms, divs);
+};
+
+function drawBackgroundSpecial_hide(divs){
+	divs.wrapper.innerHTML = '';
 	setupBackgroundSpecial();
+};
+
+function getRandomText(){
+	let arr= ['.specialTitlebar{\n   background-color:\n      rgb(94, 120, 213);\n   width: 150px;\n   height: 20px;\n   animation:\n      animExpandTitle 0.7s\n      ease-in-out;\n}'];
+	return arr[0];
 };
 
 function randomInRange(min, max){
